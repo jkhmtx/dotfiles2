7f394fb93e1126933c0ea23bee984099eea038e3
Jake Hamilton <jakehamtexas@gmail.com>
2025-03-22 19:59:26 -0500

    chore(git): basic configurattion

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -34,23 +34,6 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/jakeh/etc/profile.d/hm-session-vars.sh
-  #
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
@@ -63,6 +46,14 @@ programs.neovim = {
 
 programs.git = {
   enable = true;
+
+  userEmail = "jakehamtexas@gmail.com";
+  userName = "Jake Hamilton";
+
+  ignores = [
+    "*~"
+    "*.swp"
+  ];
 };
 
 home.sessionPath = [

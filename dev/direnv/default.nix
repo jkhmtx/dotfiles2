e528b1e4b291935b8ef23675433d4edd990154ef
Jake Hamilton <jakehamtexas@gmail.com>
2025-04-20 20:35:13 -0500

    chore(direnv): add to zsh init, use nix-direnv

--- dev/direnv/default.nix
+++ dev/direnv/default.nix
@@ -1,7 +1,9 @@
-{pkgs, ...}: {
+{...}: {
   config = {
-    home.packages = [
-      pkgs.direnv
-    ];
+    programs.direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
   };
 }
--- shell/zsh-init/default.nix
+++ shell/zsh-init/default.nix
@@ -1,15 +1,22 @@
-{pkgs, ...}:
-pkgs.writeShellApplication {
-  name = "zsh-init";
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  direnv = lib.optionals config.programs.direnv.enable [pkgs.direnv];
+in
+  pkgs.writeShellApplication {
+    name = "zsh-init";
 
-  bashOptions = [];
+    bashOptions = [];
 
-  runtimeInputs = [pkgs.oh-my-posh];
+    runtimeInputs = [pkgs.oh-my-posh] ++ direnv;
 
-  runtimeEnv = {
-    COMPLETIONS_DIR = "${../completions}";
-    OH_MY_POSH_CONFIG = "${../oh-my-posh/conf.toml}";
-  };
+    runtimeEnv = {
+      COMPLETIONS_DIR = "${../completions}";
+      OH_MY_POSH_CONFIG = "${../oh-my-posh/conf.toml}";
+    };
 
-  text = builtins.readFile ./init.sh;
-}
+    text = builtins.readFile ./init.sh;
+  }

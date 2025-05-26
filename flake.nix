3291076d52332172f534e8c1388bc1c836342c96
Jake Hamilton <jakehamtexas@gmail.com>
2025-05-26 16:12:29 -0500

    feat(core): tmux

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
           ./secrets
           ./shell
           ./term
+          ./tmux
         ];
       };
 
---
+++ tmux/default.nix
@@ -0,0 +1,44 @@
+{
+  pkgs,
+  config,
+  ...
+}: let
+  zshPackage = config.programs.zsh.package;
+
+  shell = "${zshPackage}/bin/zsh";
+in {
+  config = {
+    programs.tmux = {
+      enable = true;
+
+      baseIndex = 1;
+
+      aggressiveResize = true;
+
+      # Automatically spawn a session if trying to attach and none are running.
+      newSession = true;
+
+      shortcut = "a";
+
+      # Recommended by `require(vim.health).check()`
+      focusEvents = true;
+
+      keyMode = "vi";
+
+      inherit shell;
+      terminal = "tmux-256color";
+      historyLimit = 100000;
+
+      escapeTime = 10;
+
+      plugins = [
+        {
+          plugin = pkgs.tmuxPlugins.catppuccin;
+          extraConfig = builtins.readFile ./tmux.catppuccin.conf;
+        }
+      ];
+
+      extraConfig = builtins.readFile ./tmux.conf;
+    };
+  };
+}

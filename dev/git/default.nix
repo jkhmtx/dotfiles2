fe50c260fb2f74667db0232e3f4caba39f8bd1cb
Jake Hamilton <jakehamtexas@gmail.com>
2025-09-13 22:53:28 -0500

    feat(git): configure commit signature

--- dev/git/default.nix
+++ dev/git/default.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
   config = {
     home.packages = [pkgs.delta];
+    home.file = {
+      ".ssh/id_ed25519.pub".text = builtins.readFile ./id_ed25519.pub;
+      ".ssh/allowed_signers".text = "* ${builtins.readFile ./id_ed25519.pub}";
+    };
     programs.git = {
       enable = true;
 

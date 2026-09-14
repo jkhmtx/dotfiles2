e87aadfbbf85843d85da35f1bfedf05b1b998392
Jake Hamilton <jakehamtexas@gmail.com>
2026-09-13 21:37:01 -0500

    fix: safer ssh replacement

--- secrets/scripts/find-age-recipient/main.sh
+++ secrets/scripts/find-age-recipient/main.sh
@@ -1,14 +1,26 @@
 # shellcheck shell=bash
+eval "$(sudo op signin)"
 
-eval "$(op signin)"
+tmp="$(mktemp -d)"
 
-SSH_KEY_PATH="${HOME}/.ssh/id_ed25519-age"
-op read "op://private/nixos/private key" >"${SSH_KEY_PATH}"
-op read "op://private/nixos/public key" >"${SSH_KEY_PATH}.pub"
+trap cleanup EXIT
+
+cleanup() {
+  rm -rf "${tmp}"
+}
+
+SSH_DIR="${HOME}/.ssh"
+
+op read "op://private/nixos/private key" >"${tmp}"/pkey
+cp "${tmp}"/pkey "${SSH_DIR}"/id_ed25519-age
+cp "${tmp}"/pkey "${SSH_DIR}"/id_ed25519
+
+op read "op://private/nixos/public key" >"${tmp}"/pub
+cp "${tmp}"/pub "${SSH_DIR}/id_ed25519-age.pub"
 
 SOPS_AGE_DIR="${XDG_CONFIG_HOME:-${HOME}}/.config/sops/age"
 mkdir -p "${SOPS_AGE_DIR}"
-ssh-to-age -private-key -i "${SSH_KEY_PATH}" -o "${SOPS_AGE_DIR}/keys.txt"
+ssh-to-age -private-key -i "${SSH_DIR}/id_ed25519-age" -o "${SOPS_AGE_DIR}/keys.txt"
 
 >&2 echo "Age recipient:"
-ssh-to-age -i "${SSH_KEY_PATH}.pub"
+ssh-to-age -i "${SSH_DIR}/id_ed25519-age.pub"
